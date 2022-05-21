d6781a5e4d0f3163c058e6810468daf15646cec7
Jens Reidel <adrian@travitia.xyz>
2022-05-21 21:32:53 +0200

    Override QuickSearchBox

--- Android.mk
+++ Android.mk
@@ -29,7 +29,7 @@ LOCAL_REQUIRED_MODULES := \
         libwebviewchromium_loader \
         libwebviewchromium_plat_support
 
-LOCAL_OVERRIDES_PACKAGES := Browser2
+LOCAL_OVERRIDES_PACKAGES := Browser2 QuickSearchBox
 
 LOCAL_MODULE_TARGET_ARCH := arm arm64 x86 x86_64
 my_src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
